288068a5c07999dec8ba06fe9dfdc37786aaa5c5
abel-masila <abelmasila@gmail.com>
2017-10-06 11:37:05 +0300

    Added Css classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import './app.css';
 
 class App extends Component {
   render (){
     return (
-      <div>Coutdown Chip Component!</div>
+      <div className="App">
+        <div className="App-title">Countdown to Dec 25, 2017</div>
+        <div>
+          <div className="Clock-days">14 Days</div>
+          <div className="Clock-hours">30 Hours</div>
+          <div className="Clock-mins">15 Mins</div>
+          <div className="Clock-secs">20 Secs</div>
+        </div>
+        <div>
+          <input type="text" placeholder="New date" />
+          <button>Submit</button>
+        </div>
+      </div>
     );
   };
 }
