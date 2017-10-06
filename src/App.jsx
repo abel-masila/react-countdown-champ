b55a59a1f4ef46e3122b66b5e05efd783ab7fb19
abel-masila <abelmasila@gmail.com>
2017-10-06 11:22:31 +0300

    Setup react app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render (){
+    return (
+      <div>Coutdown Chip Component!</div>
+    );
+  };
+}
+
+ export default App;
